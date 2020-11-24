62b71f3270bd6e86a519a12b6232b4b3bf7c6b43
Aamon <33285359+44m0n@users.noreply.github.com>
2020-11-24 19:25:45 +0200

    Updated JsonNumbers.cs

--- Json/Json.Validator/JsonNumber.cs
+++ Json/Json.Validator/JsonNumber.cs
@@ -6,13 +6,26 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return IsStringADigit(input);
+            return IsStringADigit(input) && ContainLetters(input);
         }
 
         private static bool IsStringADigit(string input)
         {
             const int ZeroAscii = 48;
             return input[0] == ZeroAscii;
+        }
+
+        private static bool ContainLetters(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
