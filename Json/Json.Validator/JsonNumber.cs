670d70dac2cbbe68aeadd1188d964cd052413966
Aamon <33285359+44m0n@users.noreply.github.com>
2020-11-24 20:30:10 +0200

    Updated JsonNumber.cs

--- Json/Json.Validator/JsonNumber.cs
+++ Json/Json.Validator/JsonNumber.cs
@@ -11,6 +11,11 @@ namespace Json
 
         private static bool ContainLetters(string input)
         {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
             for (int i = 0; i < input.Length; i++)
             {
                 if (char.IsLetter(input[i]))
