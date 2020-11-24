3efc2a4285540affdda52d4ac1cbbc9280d042c1
Aamon <33285359+44m0n@users.noreply.github.com>
2020-11-24 14:39:41 +0200

    Updated JsonString.cs

--- Json/Json.Validator/JsonString.cs
+++ Json/Json.Validator/JsonString.cs
@@ -6,7 +6,27 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
+            return CheckForLength(input) && CheckForControlCharacters(input);
+        }
+
+        private static bool CheckForLength(string input)
+        {
             return (!string.IsNullOrEmpty(input) && input.Length != 1) && input[0] == '\"' && input[^1] == '\"';
+        }
+
+        private static bool CheckForControlCharacters(string input)
+        {
+            const int ControlAsciiLimit = 32;
+
+            foreach (char c in input)
+            {
+                if (c < ControlAsciiLimit)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         private static string Quoted(string input)
