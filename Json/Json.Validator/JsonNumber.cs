097cc10703d7e7e6e366a1f6dc32eac19d923207
Aamon <33285359+44m0n@users.noreply.github.com>
2020-11-24 21:58:09 +0200

    Updated JsonNumber.cs to run on all scenarios

--- Json/Json.Validator/JsonNumber.cs
+++ Json/Json.Validator/JsonNumber.cs
@@ -6,24 +6,26 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return IsValidInteger(input);
+            return CheckForForbiddenCharacters(input) && IsValidInteger(input) && IsValidExponential(input) && IsValidFractional(input);
         }
 
         private static bool IsValidInteger(string input)
         {
-            if (string.IsNullOrEmpty(input) || input.Length > 1 && (input[0] == '0' && !IsValidFractional(input)))
+            return char.IsDigit(input[0]) || input[0] == '-';
+        }
+
+        private static bool CheckForForbiddenCharacters(string input)
+        {
+            if (string.IsNullOrEmpty(input))
             {
                 return false;
             }
 
-            if (!char.IsDigit(input[0]) && input[0] != '-')
-            {
-                return false;
-            }
+            input = input.ToLower();
 
             for (int i = 1; i < input.Length; i++)
             {
-                if (!char.IsDigit(input[i]) && !IsValidFractional(input))
+                if (char.IsLetter(input[i]) && input[i] != 'e')
                 {
                     return false;
                 }
@@ -35,6 +37,12 @@ namespace Json
         private static bool IsValidFractional(string input)
         {
             int count = 0;
+
+            if (!char.IsDigit(input[^1]))
+            {
+                return false;
+            }
+
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] == '.')
@@ -43,12 +51,41 @@ namespace Json
                 }
             }
 
-            if (!char.IsDigit(input[^1]))
+            if (input.Length > 1 && input[0] == '0' && count != 1)
             {
                 return false;
             }
 
-            return count == 1;
+            return count <= 1;
+        }
+
+        private static bool IsValidExponential(string input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'e')
+                {
+                    count++;
+                }
+
+                if (input[i] == '.' && count > 0)
+                {
+                    return false;
+                }
+
+                if (i == 0)
+                {
+                    continue;
+                }
+
+                if (!char.IsDigit(input[i]) && input[i] != '-' && input[i] != '+' && input[i - 1] == 'e')
+                {
+                    return false;
+                }
+            }
+
+            return count <= 1;
         }
     }
 }
