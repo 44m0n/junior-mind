2153df8f121e8274d53dac5c4daffc21f9a38360
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-03 16:23:12 +0200

    Update RankingFacts.cs
    change the facts for the new refactored method GetPosition

--- SoccerRanking/SoccerRankingFacts/RankingFacts.cs
+++ SoccerRanking/SoccerRankingFacts/RankingFacts.cs
@@ -7,63 +7,24 @@ namespace SoccerRankingFacts
 {
     public class RankingFacts
     {
-
         [Fact]
-        public void GetRankingWithTwoTeams()
+        public void GetPositionTeamExists()
         {
-            Team[] teams = new Team[] { new Team("Cluj", 10), new Team("Alba", 20) };
-            Team[] desiredRanking = new Team[] { new Team("Alba", 20), new Team("Cluj", 10) };
+            Team[] teams = new Team[] { new Team("Alba", 20), new Team("Cluj", 20), new Team("Bucuresti", 5) };
             Ranking rank = new Ranking(teams);
-            Team[] resultedRanking = rank.GetRanking();
-            Assert.Equal(resultedRanking.ToString(), desiredRanking.ToString());
-        }
-
-        [Fact]
-        public void GetRankingWithMultipleTeams()
-        {
-            Team[] teams = new Team[] { new Team("Cluj", 10), new Team("Alba", 20), new Team("Bucuresti", 5) };
-            Team[] desiredRanking = new Team[] { new Team("Alba", 20), new Team("Cluj", 10), new Team("Bucuresti", 5) };
-            Ranking rank = new Ranking(teams);
-            Team[] resultedRanking = rank.GetRanking();
-            Assert.Equal(resultedRanking.ToString(), desiredRanking.ToString());
-        }
-
-        [Fact]
-        public void GetRankingWithOneTeam()
-        {
-            Team[] teams = new Team[] { new Team("Cluj", 10)};
-            Team[] desiredRanking = new Team[] {new Team("Cluj", 10)};
-            Ranking rank = new Ranking(teams);
-            Team[] resultedRanking = rank.GetRanking();
-            Assert.Equal(resultedRanking.ToString(), desiredRanking.ToString());
-        }
-
-        [Fact]
-        public void GetPosition()
-        {
-            Team[] teams = new Team[] { new Team("Alba", 20), new Team("Cluj", 10), new Team("Bucuresti", 5) };
-            Team desiredTeam = new Team("Cluj", 10);
-            Ranking rank = new Ranking(teams);
-            Team resultedTeam = rank.GetPosition(2);
-            Assert.Equal(resultedTeam.ToString(), desiredTeam.ToString());
-        }
-
-        [Fact]
-        public void GetPositionUsingNameWhenTheTeamExists()
-        {
-            Team[] teams = new Team[] { new Team("Alba", 20), new Team("Cluj", 10), new Team("Bucuresti", 5) };
-            Ranking rank = new Ranking(teams);
+            Team teamToFind = new Team("Cluj", 20);
             int desiredPosition = 2;
-            Assert.Equal(rank.GetPositionUsingName("Cluj"), desiredPosition);
+            Assert.Equal(rank.GetPosition(teamToFind), desiredPosition);
         }
 
         [Fact]
-        public void GetPositionUsingNameWhenTheTeamDosentExits()
+        public void GetPositionWhenTheTeamDosentExits()
         {
             Team[] teams = new Team[] { new Team("Alba", 20), new Team("Cluj", 10), new Team("Bucuresti", 5) };
             Ranking rank = new Ranking(teams);
+            Team teamToFind = new Team("Timisoara", 20);
             int desiredPosition = -1;
-            Assert.Equal(rank.GetPositionUsingName("Timisoara"), desiredPosition);
+            Assert.Equal(rank.GetPosition(teamToFind), desiredPosition);
         }
     }
 }
