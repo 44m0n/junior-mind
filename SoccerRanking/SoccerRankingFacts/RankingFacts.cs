1686ad99cadac6943da4b35ea9ea74973b6c4392
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-03 03:14:55 +0200

    Update RankingFacts.cs
    Adding new tests and scenarios.

--- SoccerRanking/SoccerRankingFacts/RankingFacts.cs
+++ SoccerRanking/SoccerRankingFacts/RankingFacts.cs
@@ -17,5 +17,25 @@ namespace SoccerRankingFacts
             Team[] resultedRanking = rank.GetRanking();
             Assert.Equal(resultedRanking.ToString(), desiredRanking.ToString());
         }
+
+        [Fact]
+        public void GetRankingWithMultipleTeams()
+        {
+            Team[] teams = new Team[] { new Team("Cluj", 10), new Team("Alba", 20), new Team("Bucuresti", 5) };
+            Team[] desiredRanking = new Team[] { new Team("Alba", 20), new Team("Cluj", 10), new Team("Bucuresti", 5) };
+            Ranking rank = new Ranking(teams);
+            Team[] resultedRanking = rank.GetRanking();
+            Assert.Equal(resultedRanking.ToString(), desiredRanking.ToString());
+        }
+
+        [Fact]
+        public void GetRankingWithOneTeam()
+        {
+            Team[] teams = new Team[] { new Team("Cluj", 10)};
+            Team[] desiredRanking = new Team[] {new Team("Cluj", 10)};
+            Ranking rank = new Ranking(teams);
+            Team[] resultedRanking = rank.GetRanking();
+            Assert.Equal(resultedRanking.ToString(), desiredRanking.ToString());
+        }
     }
 }
