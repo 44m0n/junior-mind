d041d13ff708bc2da3f49dbd8fa91ac36633f656
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-03 04:04:34 +0200

    Creating Game.cs file
    A new class was created that calculate the earned points from a game.
    The facts were updated to the new scenario.

---
+++ SoccerRanking/SoccerRanking/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TeamsConstructor;
+
+namespace GameConstructor
+{
+    public class Game
+    {
+        readonly string firstName;
+        readonly int firstScore;
+        readonly string secondName;
+        readonly int secondScore;
+
+        readonly int points = 3;
+
+        public Game(string firstName, string secondName, int firstScore, int secondScore)
+        {
+            this.firstName = firstName;
+            this.firstScore = firstScore;
+            this.secondName = secondName;
+            this.secondScore = secondScore;
+        }
+
+        public Team[] CalculatePoints()
+        {
+            // Three points for a win, one point for a draw, zero points for a lose
+            if (firstScore > secondScore)
+            {
+                return new[] { new Team(firstName, points), new Team(secondName, 0) };
+            }
+
+            if (firstScore < secondScore)
+            {
+                return new[] { new Team(firstName, 0), new Team(secondName, points) };
+            }
+
+            return new[] { new Team(firstName, 1), new Team(secondName, 1) };
+        }
+    }
+}
---
+++ SoccerRanking/SoccerRankingFacts/GameFacts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using GameConstructor;
+using TeamsConstructor;
+
+namespace SoccerRankingFacts
+{
+    public class GameFacts
+    {
+        [Fact]
+        public void CalculatePointsWithOneWinner()
+        {
+            Game game = new Game("Cluj", "Alba", 2, 0);
+            Team[] newPoints = game.CalculatePoints();
+            Team[] desiredResult = new[] { new Team("Cluj", 3), new Team("Alba", 0) };
+            Assert.Equal(newPoints.ToString(), desiredResult.ToString());
+        }
+    }
+}
