bb63d9bbc6b1d4cd920d087e1384a0294202c633
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-08 17:00:04 +0200

    Creating the constructor

--- Json/Json/Range.cs
+++ Json/Json/Range.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Json
+namespace Range
 {
     class Range
     {
+        readonly char start;
+        readonly char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
     }
 }
