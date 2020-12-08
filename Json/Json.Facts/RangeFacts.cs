40a5115c6af164f0cab6fd0a6718243d5d553a6a
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-08 17:10:07 +0200

    Creating the Range class and Match method

--- Json/Json.Facts/RangeFacts.cs
+++ Json/Json.Facts/RangeFacts.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
+using RangeConstructor;
 
 namespace Json.Facts
 {
     public class RangeFacts
     {
         [Fact]
-        public void Test1()
+        public void MatchFactsReturnTrue()
         {
+            var range = new Range('a', 'f');
+            string text = "abcd";
+            Assert.True(range.Match(text));
+        }
 
+        [Fact]
+        public void MatchFactsReturnTrueWithDigit()
+        {
+            var range = new Range('1', 'f');
+            string text = "1abcd";
+            Assert.True(range.Match(text));
+        }
+
+        [Fact]
+        public void MatchFactsReturnFalse()
+        {
+            var range = new Range('a', 'f');
+            string text = "1abcd";
+            Assert.False(range.Match(text));
         }
     }
 }
--- Json/Json/Range.cs
+++ Json/Json/Range.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Range
+namespace RangeConstructor
 {
-    class Range
+    public class Range
     {
         readonly char start;
         readonly char end;
@@ -13,6 +13,21 @@ namespace Range
         {
             this.start = start;
             this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            if (text[0] < start || text[0] > end)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
