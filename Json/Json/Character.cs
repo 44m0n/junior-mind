79006a3ccb2e986f7a0c231082950e91cc9b23fa
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-09 16:59:00 +0200

    Adding Character class

---
+++ Json/Json/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ChoiceConstructor;
+
+namespace CharacterConstructor
+{
+    class Character : IPattern
+    {
+        readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return text[0] == pattern;
+        }
+    }
+}
--- Json/Json/Range.cs
+++ Json/Json/Range.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using ChoiceConstructor;
 
 namespace RangeConstructor
 {
-    public class Range
+    public class Range : IPattern
     {
         readonly char start;
         readonly char end;
