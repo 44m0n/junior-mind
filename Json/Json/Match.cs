4ffcb4daddd392f9b53c5a728dcd409b71adcc39
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-19 17:32:55 +0200

    Create Match.cs

---
+++ Json/Json/Match.cs
@@ -0,0 +1,12 @@
+﻿namespace IMatchConstructor
+{
+    class Match
+    {
+        private readonly IMatch[] matches;
+
+        public Match(IMatch[] matches)
+        {
+            this.matches = matches;
+        }
+    }
+}
