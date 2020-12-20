30ac4e0a7b9c2c93c31760f2c8bb239e900851c0
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-20 20:01:52 +0200

    Update Range.cs

--- Json/Json/Range.cs
+++ Json/Json/Range.cs
@@ -1,4 +1,6 @@
-﻿using IPatternConstructor;
+﻿using IMatchConstructor;
+using IPatternConstructor;
+using MatchConstructor;
 
 namespace RangeConstructor
 {
@@ -13,9 +15,9 @@ namespace RangeConstructor
             this.end = end;
         }
 
-        public bool Match(string text)
+        public IMatch Match(string text)
         {
-            return !string.IsNullOrEmpty(text) && text[0] >= start && text[0] <= end;
+            return new Match(!string.IsNullOrEmpty(text) && text[0] >= start && text[0] <= end, string.IsNullOrEmpty(text) ? null : text.Substring(1));
         }
     }
 }
