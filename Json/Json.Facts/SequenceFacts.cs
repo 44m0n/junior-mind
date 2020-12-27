f5816caa2b1c6fcdac0b956e679eec77f9baac2c
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-28 00:32:18 +0200

    Creating Sequence.cs
    Creating a new class and it's facts.

---
+++ Json/Json.Facts/SequenceFacts.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using CharacterConstructor;
+using ChoiceConstructor;
+using RangeConstructor;
+using SequenceConstructor;
+
+namespace Json.Facts
+{
+    public class SequenceFacts
+    {
+        [Fact]
+        public void MatchFacts()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            Assert.True(ab.Match("abcd").Success() == true && ab.Match("abcd").RemainingText() == "cd"); // true / "cd"
+        }
+    }
+}
---
+++ Json/Json/Sequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using IMatchConstructor;
+using IPatternConstructor;
+using MatchConstructor;
+
+namespace SequenceConstructor
+{
+    public class Sequence : IPattern
+    {
+        private readonly IPattern[] patterns;
+
+        public Sequence(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            var remainingText = text;
+
+            foreach (var pattern in patterns)
+            {
+                var result = pattern.Match(remainingText);
+
+                if (result.Success())
+                {
+                    remainingText = result.RemainingText();
+                }
+                else
+                {
+                    return new Match(false, text);
+                }
+            }
+
+            return new Match(true, remainingText);
+        }
+    }
+}
