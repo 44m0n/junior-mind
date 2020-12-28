a127d47c39b6a7ff5d3249f25c8c55790747743f
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-28 04:11:32 +0200

    Creating Text.cs class

---
+++ Json/Json.Facts/TextFacts.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using TextConstructor;
+
+namespace Json.Facts
+{
+    public class TextFacts
+    {
+        [Fact]
+        public void MatchFacts()
+        {
+            var tr = new Ttext("true");
+
+            Assert.True(tr.Match("true").Success() && tr.Match("true").RemainingText() == ""); // true / ""
+            Assert.True(tr.Match("trueX").Success() && tr.Match("trueX").RemainingText() == "X"); // true / "X"
+            Assert.True(!tr.Match("false").Success() && tr.Match("false").RemainingText() == "false"); // false / "false"
+            Assert.True(!tr.Match("").Success() && tr.Match("").RemainingText() == ""); // false / ""
+            Assert.True(!tr.Match(null).Success() && tr.Match(null).RemainingText() == null); // false / null
+
+            var empty = new Ttext("");
+            Assert.True(empty.Match("true").Success() && empty.Match("true").RemainingText() == "true");
+            Assert.True(!empty.Match(null).Success() && empty.Match(null).RemainingText() == null);
+        }
+    }
+}
---
+++ Json/Json/Text.cs
@@ -0,0 +1,34 @@
+﻿using IMatchConstructor;
+using IPatternConstructor;
+using MatchConstructor;
+
+namespace TextConstructor
+{
+    // Text (and txt) conflicts with System.Drawing.Text
+    public class Ttext : IPattern
+    {
+        readonly string prefix;
+
+        public Ttext(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            if (prefix == "")
+            {
+                return new Match(true, text);
+            }
+
+            bool prefixExists = text.StartsWith(prefix);
+
+            return new Match(prefixExists, prefixExists ? text.Substring(prefix.Length) : text);
+        }
+    }
+}
