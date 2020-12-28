29b70601ac3231c75061c4cb30686e30ff26f9c3
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-28 02:05:05 +0200

    Creating Any.cs class and it's facts.

---
+++ Json/Json.Facts/AnyFacts.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using AnyConstructor;
+
+namespace Json.Facts
+{
+    public class AnyFacts
+    {
+        [Fact]
+        public void MatchFacts()
+        {
+            var e = new Any("eE");
+
+            Assert.True(e.Match("ea").Success() && e.Match("ea").RemainingText() == "a"); // true / "a"
+            Assert.True(e.Match("Ea").Success() && e.Match("ea").RemainingText() == "a"); // true / "a"
+            Assert.True(!e.Match("a").Success() && e.Match("a").RemainingText() == "a"); // false / "a"
+            Assert.True(!e.Match("").Success() && e.Match("").RemainingText() == "");    // false / ""
+            Assert.True(!e.Match(null).Success() && e.Match(null).RemainingText() == null); // false / null
+
+            var sign = new Any("-+");
+
+            Assert.True(sign.Match("+3").Success() && sign.Match("+3").RemainingText() == "3"); // true / "3"
+            Assert.True(sign.Match("-2").Success() && sign.Match("-2").RemainingText() == "2"); // true / "2"
+            Assert.True(!sign.Match("2").Success() && sign.Match("2").RemainingText() == "2"); // true / "2"
+            Assert.True(!sign.Match("").Success() && sign.Match("").RemainingText() == ""); // true / ""
+            Assert.True(!sign.Match(null).Success() && sign.Match(null).RemainingText() == null); // true / null
+        } 
+    }
+}
---
+++ Json/Json/Any.cs
@@ -0,0 +1,34 @@
+﻿using IMatchConstructor;
+using IPatternConstructor;
+using MatchConstructor;
+
+namespace AnyConstructor
+{
+    public class Any : IPattern
+    {
+        readonly string letters;
+
+        public Any(string letters)
+        {
+            this.letters = letters;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            foreach (char c in letters)
+            {
+                if (text[0] == c)
+                {
+                    return new Match(true, text.Substring(1));
+                }
+            }
+
+            return new Match(false, text);
+        }
+    }
+}
