48fbce7c198b85ab2a829b2889721fb02a76cc7d
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-28 04:29:46 +0200

    Creating Many.cs class and it's facts

---
+++ Json/Json.Facts/ManyFacts.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using ManyConstructor;
+using CharacterConstructor;
+using RangeConstructor;
+
+namespace Json.Facts
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void MatchFacts()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+            Assert.Equal("bc", a.Match("aaaabc").RemainingText());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+            Assert.Equal("", a.Match("").RemainingText());
+            Assert.True(a.Match(null).RemainingText() == null);
+
+            var digits = new Many(new Range('0', '9'));
+
+            Assert.Equal("ab123", digits.Match("12345ab123").RemainingText());
+            Assert.Equal("ab", digits.Match("ab").RemainingText());
+        }
+    }
+}
---
+++ Json/Json/Many.cs
@@ -0,0 +1,34 @@
+﻿using IMatchConstructor;
+using IPatternConstructor;
+using MatchConstructor;
+
+namespace ManyConstructor
+{
+    public class Many : IPattern
+    {
+        readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(true, text);
+            }
+
+            var match = pattern.Match(text);
+
+            while (match.Success())
+            {
+                text = match.RemainingText();
+                match = pattern.Match(text);
+            }
+
+            return new Match(true, text);
+        }
+    }
+}
