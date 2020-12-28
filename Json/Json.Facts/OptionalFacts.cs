424fd7b4b16694c5885a7ad3b1f2ed42f50d6dc9
Aamon <33285359+44m0n@users.noreply.github.com>
2020-12-28 20:25:50 +0200

    Creating Optional.cs class and it's tests

---
+++ Json/Json.Facts/OptionalFacts.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using CharacterConstructor;
+using OptionalConstructor;
+
+namespace Json.Facts
+{
+    public class OptionalFacts
+    {
+        [Fact]
+        public void MatchFacts()
+        {
+            var a = new Optional(new Character('a'));
+
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+            Assert.Equal("abc", a.Match("aabc").RemainingText());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+            Assert.Equal("", a.Match("").RemainingText());
+            Assert.True(a.Match(null).RemainingText() == null);
+
+            var sign = new Optional(new Character('-'));
+
+            Assert.Equal("123", sign.Match("123").RemainingText());
+            Assert.Equal("123", sign.Match("-123").RemainingText());
+        }
+    }
+}
---
+++ Json/Json/Optional.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using IMatchConstructor;
+using IPatternConstructor;
+using MatchConstructor;
+
+namespace OptionalConstructor
+{
+#pragma warning disable CA1716 // Identifiers should not match keywords
+    public class Optional : IPattern
+#pragma warning restore CA1716 // Identifiers should not match keywords
+    {
+        readonly IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(true, text);
+            }
+
+            var result = pattern.Match(text);
+
+            return new Match(true, result.RemainingText());
+        }
+    }
+}
