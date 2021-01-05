6256546d44adb00d026f9c9d7d7207ca81dcabd2
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-05 17:09:13 +0200

    Create Number.cs

---
+++ Json/Json/Number.cs
@@ -0,0 +1,54 @@
+﻿namespace Json
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            /* digits:
+             *      digit:
+             *          0-9
+             *          1-9
+             *      digit digits
+             */
+            IPattern digit = new Range('0', '9');
+            IPattern digits = new OneOrMore(digit);
+            IPattern oneNineDigit = new Range('1', '9');
+
+            /* integer:
+            *      digit
+            *      onenine digits
+            *      '-' digit
+            *      '-' onenine digits
+            */
+            IPattern integer = new Choice(
+                digit,
+                new Sequence(oneNineDigit, digits),
+                new Sequence(new Character('-'), digit),
+                new Sequence(new Character('-'), oneNineDigit, digits));
+
+            // fraction: "" || '.' digits
+            IPattern fraction = new Optional(new Choice(new Sequence(new Character('.'), digits)));
+
+            // sign: "" || '+' || '-'
+            IPattern sign = new Optional(
+                new Choice(
+                    new Character('+'),
+                    new Character('-')));
+
+            // exponent: "" || 'e' sign digits
+            IPattern expo = new Optional(
+                new Choice(
+                    new Sequence(new Character('e'), sign, digits),
+                    new Sequence(new Character('E'), sign, digits)));
+
+            pattern = new Sequence(integer, fraction, expo);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
