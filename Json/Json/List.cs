be7f9e7e870cbff79c42edbabd71e39827f4545a
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-05 14:39:42 +0200

    Update List.cs
    Solved some errors

--- Json/Json/List.cs
+++ Json/Json/List.cs
@@ -12,7 +12,7 @@ namespace ListConstructor
 
         public List(IPattern element, IPattern separator)
         {
-            this.pattern = new Optional(new Sequence(element, new Many(new Sequence(element, separator))));
+            this.pattern = new Optional(new Sequence(element, new Many(new Sequence(separator, element))));
         }
 
         public IMatch Match(string text)
