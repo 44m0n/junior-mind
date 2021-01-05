e90e0bdc149ffa6a39dd9ee8766a116b378f1a55
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-05 14:14:58 +0200

    Create List.cs

---
+++ Json/Json/List.cs
@@ -0,0 +1,23 @@
+﻿using IMatchConstructor;
+using IPatternConstructor;
+using ManyConstructor;
+using OptionalConstructor;
+using SequenceConstructor;
+
+namespace ListConstructor
+{
+    public class List
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Optional(new Sequence(element, new Many(new Sequence(element, separator))));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
