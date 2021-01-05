1ba52de1b85751f1f9fa30f8be9ae998be05e4f8
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-05 13:41:42 +0200

    Added OneOrMore class and its facts

---
+++ Json/Json.Facts/OneOrMoreFacts.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using RangeConstructor;
+using OneOrMoreConstructor;
+
+namespace Json.Facts
+{
+    public class OneOrMoreFacts
+    {
+        [Fact]
+        public void MatchFacts()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            Assert.True(a.Match("123").Success() && a.Match("123").RemainingText() == "");
+            Assert.True(a.Match("1a").Success() && a.Match("1a").RemainingText() == "a");
+            Assert.True(!a.Match("bc").Success() && a.Match("bc").RemainingText() == "bc");
+            Assert.True(!a.Match("").Success() && a.Match("").RemainingText() == "");
+            Assert.True(!a.Match(null).Success() && a.Match(null).RemainingText() == null);
+        }
+    }
+}
---
+++ Json/Json/OneOrMore.cs
@@ -0,0 +1,22 @@
+﻿using IMatchConstructor;
+using IPatternConstructor;
+using ManyConstructor;
+using SequenceConstructor;
+
+namespace OneOrMoreConstructor
+{
+    public class OneOrMore
+    {
+        readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Sequence(pattern, new Many(pattern));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
