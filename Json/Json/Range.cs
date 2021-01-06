1851d4ca15fd27a04a9ee2d3d0435615b1e59a0f
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-06 15:57:23 +0200

    Update Range.cs
    Refactoring the code

--- Json/Json/Range.cs
+++ Json/Json/Range.cs
@@ -20,7 +20,9 @@
                 return new Match(false, text);
             }
 
-            return (text[0] >= start && text[0] <= end) && !excluded.Contains(text[0]) ? new Match(true, text.Substring(1)) : new Match(false, text);
+            return (text[0] >= start && text[0] <= end) && !excluded.Contains(text[0])
+                ? new Match(true, text.Substring(1))
+                : new Match(false, text);
         }
     }
 }
