0d47e9d565d8ba1ea707593e03e36ba4df438ae9
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-06 15:44:42 +0200

    Update Range.cs
    Adding a new feature where a char can be skipped in the range

--- Json/Json/Range.cs
+++ Json/Json/Range.cs
@@ -4,11 +4,13 @@
     {
         readonly char start;
         readonly char end;
+        readonly string excluded;
 
-        public Range(char start, char end)
+        public Range(char start, char end, string excluded = "")
         {
             this.start = start;
             this.end = end;
+            this.excluded = excluded;
         }
 
         public IMatch Match(string text)
@@ -18,7 +20,7 @@
                 return new Match(false, text);
             }
 
-            return text[0] >= start && text[0] <= end ? new Match(true, text.Substring(1)) : new Match(false, text);
+            return (text[0] >= start && text[0] <= end) && !excluded.Contains(text[0]) ? new Match(true, text.Substring(1)) : new Match(false, text);
         }
     }
 }
