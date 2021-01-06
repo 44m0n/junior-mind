62e93b26c8733653d6cb32fca4a5369d076a75c5
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-06 15:14:59 +0200

    Update Number.cs
    Refactoring the code

--- Json/Json/Number.cs
+++ Json/Json/Number.cs
@@ -6,33 +6,15 @@
 
         public Number()
         {
-            /* digits:
-             *      digit:
-             *          0-9
-             *          1-9
-             *      digit digits
-             */
             var digit = new Range('0', '9');
             var digits = new OneOrMore(digit);
-
-            /* integer:
-            *      digit
-            *      onenine digits
-            *      '-' digit
-            *      '-' onenine digits
-            */
 
             var integer = new Sequence(
                 new Optional(new Character('-')),
                 new Choice(new Character('0'), digits));
 
-            // fraction: "" || '.' digits
             var fraction = new Optional(new Choice(new Sequence(new Character('.'), digits)));
-
-            // sign: "" || '+' || '-'
             var sign = new Optional(new Any("-+"));
-
-            // exponent: "" || 'e' sign digits
             var expo = new Optional(new Sequence(new Any("eE"), sign, digits));
 
             pattern = new Sequence(integer, fraction, expo);
