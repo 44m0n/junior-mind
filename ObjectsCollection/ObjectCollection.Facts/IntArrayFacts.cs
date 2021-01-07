7eddc1d45411b612ab2b4e2b45259d733bfa50e8
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-07 17:39:38 +0200

    Update

---
+++ ObjectsCollection/ObjectCollection.Facts/IntArrayFacts.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ObjectCollection.Facts
+{
+    public class IntArrayFacts
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
