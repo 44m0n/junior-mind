628c2ab3fbd032aa5c8af29af70a76a88c681c54
Aamon <33285359+44m0n@users.noreply.github.com>
2021-01-07 17:12:50 +0200

    Update IntArray.cs

--- ObjectsCollection/ObjectsCollection/IntArray.cs
+++ ObjectsCollection/ObjectsCollection/IntArray.cs
@@ -17,6 +17,34 @@ namespace ObjectsCollection
             array[^1] = element;
         }
 
+        public int Count()
+        {
+            return array.Length;
+        }
+
+        public int Element(int index)
+        {
+            return array[index];
+        }
+
+        public void SetElement(int index, int element)
+        {
+            array[index] = element;
+        }
+
+        public bool Contains(int element)
+        {
+            foreach (var el in array)
+            {
+                if (array[el] == element)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
 
     }
 }
