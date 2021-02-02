6d87679d4d5440026a4c4995933aeab6820844ca
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-02 19:51:04 +0200

    Create SortedIntArray.cs
    A new functionality has been created. The asserts have been updated to the new scenario.

---
+++ ObjectsCollection/ObjectCollection.Facts/SortedArrayFacts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ObjectsCollection;
+using Xunit;
+
+namespace ObjectsCollection.Facts
+{
+    public class SortedArrayFacts
+    {
+        [Fact]
+        public void SortedArray()
+        {
+            var obj = new SortedIntArray();
+
+            Assert.True(obj.Count == 0);
+
+            obj[0] = 5;
+            obj[0] = 4;
+            Assert.True(obj[0] == 4);
+            Assert.True(obj.Count == 1);
+
+            obj.Add(3);
+            Assert.True(obj.Count == 2);
+            Assert.True(obj[0] == 3);
+            Assert.True(obj[1] == 4);
+
+            obj.Insert(0, 5);
+            Assert.True(obj.Count == 3);
+            Assert.True(obj[0] == 3);
+            Assert.True(obj[1] == 4);
+            Assert.True(obj[2] == 5);
+
+            obj.Remove(4);
+            Assert.Equal(2, obj.Count);
+            Assert.Equal(5, obj[1]);
+
+            obj[1] = 1;
+            Assert.Equal(3, obj[1]);
+        }
+    }
+}
---
+++ ObjectsCollection/ObjectsCollection/SortedIntArray.cs
@@ -0,0 +1,50 @@
+﻿namespace ObjectsCollection
+{
+    public class SortedIntArray : IntArray
+    {
+        public SortedIntArray()
+        {
+        }
+
+        public override int this[int index]
+        {
+            get => base[index];
+            set
+            {
+                var no = base[index];
+                Add(value);
+                Remove(no);
+            }
+        }
+
+        public override void Add(int element)
+        {
+            Sort(element);
+        }
+
+        public override void Insert(int index, int element)
+        {
+            Add(element);
+        }
+
+        private void Sort(int element)
+        {
+            if (Count == 0)
+            {
+                base.Add(element);
+                return;
+            }
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (element < this[i])
+                {
+                    base.Insert(i, element);
+                    return;
+                }
+            }
+
+            base.Add(element);
+        }
+    }
+}
