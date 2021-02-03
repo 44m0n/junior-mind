27894a960347276442a1dc471738a47355159027
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-04 01:41:57 +0200

    Create IEnumerator.cs
    Creating IEnumerator because we need to implement IEnumerable

---
+++ ObjectsCollection/ObjectsCollection/IEnumerator.cs
@@ -0,0 +1,28 @@
+﻿namespace ObjectsCollection
+{
+    class IEnumerator
+    {
+        private readonly ObjectArray array;
+        private readonly int count;
+        private int position = -1;
+
+        public IEnumerator(ObjectArray array, int count)
+        {
+            this.array = array;
+            this.count = count;
+        }
+
+        object Current { get => array[position]; }
+
+        public bool MoveNext()
+        {
+            position++;
+            return position < count;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
