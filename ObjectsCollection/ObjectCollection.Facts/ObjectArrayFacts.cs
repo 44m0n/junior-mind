4601fbdc09629ed49286232ec37142e1f1c6674f
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-03 14:28:56 +0200

    Create ObjectArray.cs
    Created another class with its facts.

---
+++ ObjectsCollection/ObjectCollection.Facts/ObjectArrayFacts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Xunit;
+using ObjectsCollection;
+
+namespace ObjectsCollection.Facts
+{
+    public class ObjectArrayFacts
+    {
+        [Fact]
+        public void ObjectArrayFact()
+        {
+            var obj = new ObjectArray();
+
+            object obj1 = 5;
+
+            Assert.True(obj.Count == 0);
+
+            obj.Add(obj1);
+            Assert.True(obj.IndexOf(obj1) == 0);
+            Assert.True(obj.Count == 1);
+
+            Assert.Equal(obj1, obj[0]);
+
+            object obj2 = 3;
+            obj.Add(obj2);
+            Assert.True(obj.Count == 2);
+            Assert.True(obj.IndexOf(obj2) == 1);
+            Assert.Equal(obj2, obj[1]);
+
+            object obj3 = 2;
+            obj[0] = obj3;
+            Assert.True(obj.IndexOf(obj3) == 0);
+
+            
+            obj.Remove(obj3);
+            Assert.True(obj.Count == 1);
+            Assert.True(obj.IndexOf(obj2) == 0);
+            Assert.True(obj.Contains(obj2));
+
+            obj3 = 1;
+            obj.Insert(0, obj3);
+            Assert.True(obj.IndexOf(obj3) == 0);
+            Assert.True(obj.IndexOf(obj2) == 1);
+
+            obj.Remove(obj3);
+            Assert.True(obj.IndexOf(obj2) == 0);
+            Assert.True(obj.Count == 1);
+
+
+            obj1 = "string";
+            obj.Add(obj1);
+            Assert.True(obj.IndexOf(obj1) == 1);
+
+            obj1 = String.Empty;
+            obj.Add(obj1);
+            Assert.True(obj[2] == String.Empty);
+            obj.Remove(String.Empty);
+            Assert.True(obj.Count == 2);
+
+            obj1 = false;
+            obj.Add(obj1);
+            Assert.True((bool)obj[2] == false);
+
+            obj1 = 3.1;
+            obj.Add(obj1);
+            Assert.True((double)obj[3] == 3.1);
+        }
+    }
+}
---
+++ ObjectsCollection/ObjectsCollection/ObjectArray.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace ObjectsCollection
+{
+    public class ObjectArray
+    {
+        private const int ActualLength = 4;
+        private object[] array;
+
+        public ObjectArray()
+        {
+            array = new object[ActualLength];
+        }
+
+        public int Count { get; private set; }
+
+        public virtual object this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public void Add(object element)
+        {
+            Count++;
+            CheckActualLength();
+            array[Count - 1] = element;
+        }
+
+        public bool Contains(object element)
+        {
+            return IndexOf(element) != -1;
+        }
+
+        public int IndexOf(object element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, object element)
+        {
+            ShiftRight(index);
+            array[index] = element;
+        }
+
+        public void Clear()
+        {
+            array = new object[ActualLength];
+            Count = 0;
+        }
+
+        public void Remove(object element)
+        {
+            var index = IndexOf(element);
+            if (index == -1)
+            {
+                return;
+            }
+
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+        }
+
+        private void ShiftRight(int index)
+        {
+            Count++;
+            UpdateArrayLength();
+
+            for (int i = Count - 1; i > index; i--)
+            {
+                Swap(i, i - 1);
+            }
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                Swap(i, i + 1);
+            }
+
+            Count--;
+        }
+
+        private void Swap(int i, int j)
+        {
+            array[i] = array[j];
+        }
+
+        private void UpdateArrayLength()
+        {
+            const int doubleLength = 2;
+            Array.Resize(ref array, array.Length * doubleLength);
+        }
+
+        private void CheckActualLength()
+        {
+            if (Count <= array.Length)
+            {
+                return;
+            }
+
+            UpdateArrayLength();
+        }
+    }
+}
