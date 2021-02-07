1f423bf9c6b8363ad914fdd814e0c92a2be3b7f5
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-07 17:08:49 +0200

    Ccreating List.cs

---
+++ ObjectsCollection/ObjectCollection.Facts/ListFacts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ObjectsCollection.Facts
+{
+    public class ListFacts
+    {
+        [Fact]
+        public void ListFact()
+        {
+            List<int> obj = new List<int>();
+
+            Assert.True(obj.Count == 0);
+
+            obj.Add(5);
+            Assert.True(obj.IndexOf(5) == 0);
+            Assert.True(obj.Count == 1);
+
+            Assert.True(obj[0] == 5);
+
+            obj.Add(3);
+            Assert.True(obj.Count == 2);
+            Assert.True(obj.IndexOf(3) == 1);
+            Assert.True(obj[1] == 3);
+
+            obj[0] = 2;
+            Assert.True(obj.IndexOf(2) == 0);
+
+            obj.Remove(2);
+            Assert.True(obj.Count == 1);
+            Assert.True(obj.IndexOf(3) == 0);
+            Assert.True(obj.Contains(3));
+
+            obj.Insert(0, 1);
+            Assert.True(obj.IndexOf(1) == 0);
+            Assert.True(obj.IndexOf(3) == 1);
+
+            obj.Remove(1);
+            Assert.True(obj.IndexOf(3) == 0);
+            Assert.True(obj.Count == 1);
+        }
+
+    }
+}
---
+++ ObjectsCollection/ObjectsCollection/List.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ObjectsCollection
+{
+    public class List<T> : IEnumerable<T>
+    {
+        private const int ActualLength = 4;
+        private T[] array;
+
+        public List()
+        {
+            array = new T[ActualLength];
+        }
+
+        public int Count { get; private set; }
+
+        public virtual T this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T element)
+        {
+            Count++;
+            CheckActualLength();
+            array[Count - 1] = element;
+        }
+
+        public bool Contains(T element)
+        {
+            return IndexOf(element) != -1;
+        }
+
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T element)
+        {
+            ShiftRight(index);
+            array[index] = element;
+        }
+
+        public void Clear()
+        {
+            array = new T[ActualLength];
+            Count = 0;
+        }
+
+        public void Remove(T element)
+        {
+            var index = IndexOf(element);
+            if (index == -1)
+            {
+                return;
+            }
+
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+        }
+
+        private void ShiftRight(int index)
+        {
+            Count++;
+            UpdateArrayLength();
+
+            for (int i = Count - 1; i > index; i--)
+            {
+                Swap(i, i - 1);
+            }
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                Swap(i, i + 1);
+            }
+
+            Count--;
+        }
+
+        private void Swap(int i, int j)
+        {
+            array[i] = array[j];
+        }
+
+        private void UpdateArrayLength()
+        {
+            const int doubleLength = 2;
+            Array.Resize(ref array, array.Length * doubleLength);
+        }
+
+        private void CheckActualLength()
+        {
+            if (Count <= array.Length)
+            {
+                return;
+            }
+
+            UpdateArrayLength();
+        }
+    }
+}
