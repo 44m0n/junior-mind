ead3652ede74d53854f4435ee1cacd680c5a1fa8
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-07 17:32:31 +0200

    Create SortedList.cs and its facts

---
+++ ObjectsCollection/ObjectCollection.Facts/SortedListFacts.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+
+namespace ObjectsCollection.Facts
+{
+    public class SortedListFacts
+    {
+        [Fact]
+        public void SortedList()
+        {
+            var obj = new SortedList<int>();
+
+            Assert.True(obj.Count == 0);
+
+            obj.Add(5);
+            Assert.True(obj[0] == 5);
+            obj[0] = 4;
+            Assert.True(obj[0] == 4);
+            Assert.True(obj.Count == 1);
+
+
+            obj.Add(3);
+            Assert.True(obj.Count == 2);
+            Assert.True(obj[0] == 3);
+            Assert.True(obj[1] == 4);
+
+
+            obj.Insert(0, 5);
+            Assert.True(obj.Count == 2);
+            Assert.True(obj[0] == 3);
+            Assert.True(obj[1] == 4);
+            Assert.False(obj[0] == 5);
+
+
+            obj.Remove(4);
+            Assert.Equal(1, obj.Count);
+            Assert.Equal(3, obj[0]);
+
+            obj[0] = 1;
+            Assert.Equal(1, obj[0]);
+
+            obj.Add(4);
+            Assert.True(obj.Count == 2);
+            obj.Insert(1, 2);
+            Assert.True(obj.Count == 3);
+            Assert.True(obj[1] == 2);
+            obj[2] = 3;
+            Assert.True(obj[2] == 3);
+            obj[2] = 1;
+            Assert.False(obj[2] == 1);
+
+        }
+    }
+}
---
+++ ObjectsCollection/ObjectsCollection/SortedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ObjectsCollection
+{
+    public class SortedList<T> : List<T>
+        where T : IComparable<T>
+    {
+        public SortedList() : base()
+        {
+        }
+
+        public override T this[int index]
+        {
+            get => base[index];
+            set
+            {
+                if (ElementAtOrDefault(index - 1, value).CompareTo(value) == 1 || value.CompareTo(ElementAtOrDefault(index + 1, value)) == 1)
+                {
+                    return;
+                }
+
+                base[index] = value;
+            }
+        }
+
+        public override void Add(T element)
+        {
+            Sort(element);
+        }
+
+        public override void Insert(int index, T element)
+        {
+            if (ElementAtOrDefault(index - 1, element).CompareTo(element) == 1 || element.CompareTo(ElementAtOrDefault(index, element)) == 1)
+            {
+                return;
+            }
+
+            base.Insert(index, element);
+        }
+
+        private void Sort(T element)
+        {
+            if (Count == 0)
+            {
+                base.Add(element);
+                return;
+            }
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (element.CompareTo(this[i]) == -1)
+                {
+                    base.Insert(i, element);
+                    return;
+                }
+            }
+
+            base.Add(element);
+        }
+
+        private T ElementAtOrDefault(int index, T value)
+        {
+            return index < 0 || index >= Count ? value : this[index];
+        }
+    }
+}
