756297fa3c3a2283fa09a52a03f1392d50f7f7e2
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-10 16:28:10 +0200

    Update LinkedList.cs
    Implementing First and Last proprieties.

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -16,9 +16,13 @@ namespace LinkedList
             sentinel.IsLinkedTo(sentinel, sentinel);
         }
 
+        public bool IsReadOnly { get; }
+
         public int Count { get; private set; }
 
-        public bool IsReadOnly { get; }
+        public LinkedListNode<T> First => Count != 0 ? sentinel.Next : null;
+
+        public LinkedListNode<T> Last => Count != 0 ? sentinel.Previous : null;
 
         public void Add(T item)
         {
