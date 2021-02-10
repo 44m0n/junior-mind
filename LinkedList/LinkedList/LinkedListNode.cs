7f77232636116d64354268393b7b459985020c52
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-10 15:46:58 +0200

    Create LinkedListNode.cs
    Creating Nodes for LinkedList

---
+++ LinkedList/LinkedList/LinkedListNode.cs
@@ -0,0 +1,24 @@
+﻿namespace LinkedList
+{
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(T value)
+        {
+            Value = value;
+            Previous = null;
+            Next = null;
+        }
+
+        public T Value { get; private set; }
+
+        public LinkedListNode<T> Next { get; private set; }
+
+        public LinkedListNode<T> Previous { get; private set; }
+
+        public void IsLinkedTo(LinkedListNode<T> previous, LinkedListNode<T> next)
+        {
+            Previous = previous;
+            Next = next;
+        }
+    }
+}
