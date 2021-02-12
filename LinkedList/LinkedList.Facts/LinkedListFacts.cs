24b0a3ce451b0995be69c1bc027553be0fdda93b
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-12 12:11:46 +0200

    Update LinkedListFacts.cs
    Adding new facts.

--- LinkedList/LinkedList.Facts/LinkedListFacts.cs
+++ LinkedList/LinkedList.Facts/LinkedListFacts.cs
@@ -49,5 +49,31 @@ namespace LinkedList.Facts
             Assert.Equal(2, list.First.Value);
             Assert.Equal(2, list.Last.Value);
         }
+
+        [Fact]
+        public void AddFactNodeReturnException()
+        {
+            LinkedListCollection<int> list = new LinkedListCollection<int>();
+            LinkedListNode<int> node = new LinkedListNode<int>(2, default);
+            var err = Assert.Throws<InvalidOperationException>(() => list.Add(node));
+            Assert.Equal("The node is not in the current list or belongs to another list", err.Message);
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void AddAfter()
+        {
+            LinkedListCollection<int> list = new LinkedListCollection<int>() { 1, 2, 3 };
+
+            var node = list.Last;
+            list.Add(4);
+
+            list.AddAfter(5, node);
+            Assert.Equal(5, list.Count);
+            Assert.Equal(4, list.Last.Value);
+            list.RemoveLast();
+            Assert.Equal(4, list.Count);
+            Assert.Equal(5, list.Last.Value);
+        }
     }
 }
