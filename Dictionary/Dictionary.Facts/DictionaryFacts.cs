baf6c394c3fed7fbbf550d3367827eb9e818e81a
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-16 15:55:46 +0200

    Update DictionaryFacts.cs

--- Dictionary/Dictionary.Facts/DictionaryFacts.cs
+++ Dictionary/Dictionary.Facts/DictionaryFacts.cs
@@ -1,6 +1,6 @@
 using System;
 using Xunit;
-using Dictionary;
+using System.Collections.Generic;
 
 namespace Dictionary.Facts
 {
@@ -9,15 +9,34 @@ namespace Dictionary.Facts
         [Fact]
         public void AddFact()
         {
-            var dict = new Dictionary.DictionaryObject<int, string>();
+            var dict = new DictionaryObject<int, string>();
             dict.Add(1, "ceva");
+            Assert.True(dict[1] == "ceva");
             Assert.Equal(1, dict.Count);
+        }
+
+        [Fact]
+        public void AddFactPair()
+        {
+            var dict = new DictionaryObject<int, string>() { { 1, "a" }, { 2, "b" }, { 3, "c" } };
+            dict.Add((new KeyValuePair<int, string>(5, "b")));
+            Assert.Equal(4, dict.Count);
+            Assert.Equal("b", dict[5]);
+        }
+
+        [Fact]
+        public void AddFactPairWithConflicts()
+        {
+            var dict = new DictionaryObject<int, string>() { { 1, "a" }, { 2, "b" }, { 3, "c" } };
+            dict.Add((new KeyValuePair<int, string>(6, "b")));
+            Assert.Equal(4, dict.Count);
+            Assert.Equal("b", dict[6]);
         }
 
         [Fact]
         public void IndexGet()
         {
-            var dict = new Dictionary.DictionaryObject<int, string>();
+            var dict = new DictionaryObject<int, string>();
             dict.Add(1, "ceva");
             Assert.Equal("ceva", dict[1]);
         }
@@ -25,7 +44,7 @@ namespace Dictionary.Facts
         [Fact]
         public void IndexSet()
         {
-            var dict = new Dictionary.DictionaryObject<int, string>();
+            var dict = new DictionaryObject<int, string>();
             dict.Add(1, "ceva");
             dict[1] = "altceva";
             Assert.Equal("altceva", dict[1]);
@@ -34,19 +53,48 @@ namespace Dictionary.Facts
         [Fact]
         public void ClearFacts()
         {
-            var dict = new Dictionary.DictionaryObject<int, string>();
+            var dict = new DictionaryObject<int, string>();
             dict.Add(1, "ceva");
             Assert.Equal(1, dict.Count);
             dict.Clear();
             Assert.Equal(0, dict.Count);
         }
 
+        [Fact]
         public void ContainsFacts()
         {
-            var dict = new Dictionary.DictionaryObject<int, string>();
+            var dict = new DictionaryObject<int, string>();
             dict.Add(1, "ceva");
 
             Assert.True(dict.ContainsKey(1));
+            Assert.False(dict.ContainsKey(2));
+        }
+
+        [Fact]
+        public void ContainsPair()
+        {
+            var dict = new DictionaryObject<int, string>() { { 1, "a" }, { 2, "b" } };
+            Assert.True(dict.Contains(new KeyValuePair<int, string>(1, "a")));
+            Assert.False(dict.Contains(new KeyValuePair<int, string>(5, "b")));
+        }
+
+        [Fact]
+        public void Remove()
+        {
+            var dict = new DictionaryObject<int, string>() { { 1, "a" }, { 2, "b" } };
+            dict.Remove(1);
+            Assert.False(dict.Contains(new KeyValuePair<int, string>(1, "a")));
+        }
+
+        [Fact]
+        public void RemoveWithConflicts()
+        {
+            var dict = new DictionaryObject<int, string>() { { 1, "a" }, { 6, "b" }, { 3, "c" } };
+            Assert.Equal(3, dict.Count);
+            dict.Remove(6);
+            Assert.Equal(2, dict.Count);
+            Assert.True(dict.Contains(new KeyValuePair<int, string>(1, "a")));
+            Assert.False(dict.Contains(new KeyValuePair<int, string>(6, "b")));
         }
     }
 }
