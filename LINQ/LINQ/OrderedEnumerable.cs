3e87e774b31e057f517f3a5ea95ce2a68545b518
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-23 14:28:20 +0200

    Creating new class

---
+++ LINQ/LINQ/OrderedEnumerable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class OrderedEnumerable<TSource, TKey> : IOrderedEnumerable<TSource>
+    {
+        private readonly IEnumerable<TSource> source;
+        private readonly Func<TSource, TKey> keySelector;
+        private readonly IComparer<TKey> comparer;
+
+        public OrderedEnumerable(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IComparer<TKey> comparer)
+        {
+            this.source = source;
+            this.keySelector = keySelector;
+            this.comparer = comparer;
+        }
+
+        public IOrderedEnumerable<TSource> CreateOrderedEnumerable<TKey1>(Func<TSource, TKey1> keySelector, IComparer<TKey1> comparer, bool descending)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<TSource> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
