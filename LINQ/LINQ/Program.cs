ae2ab87d60fd113c80bcee269cfdfec852210959
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-24 16:56:27 +0200

    Refactoring

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -3,28 +3,10 @@ using System.Collections.Generic;
 
 namespace LINQ
 {
-    struct Person
-    {
-        public string Name { get; set; }
-
-        public int Age { get; set; }
-    }
-
     public class Program
     {
         static void Main()
         {
-            Person[] people =
-            {
-                new Person { Name = "John", Age = 8 },
-                new Person { Name = "Johnny", Age = 4 },
-                new Person { Name = "Johannes", Age = 1 }
-            };
-
-            foreach (var el in people.OrderBy(e => e.Age, Comparer<int>.Default))
-            {
-                Console.WriteLine(el.Name);
-            }
         }
     }
 }
