2d771e2840e76dda79c36e53a7320752e07e80c2
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-25 15:36:28 +0200

    Create ProjectionComparer.cs

---
+++ LINQ/LINQ/ProjectionComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LINQ
+{
+#pragma warning disable RCS1241 // Implement non-generic counterpart.
+    public class ProjectionComparer<TSource, TKey> : IComparer<TSource>
+#pragma warning restore RCS1241 // Implement non-generic counterpart.
+    {
+        private readonly Func<TSource, TKey> keySelector;
+        private readonly IComparer<TKey> comparer;
+
+        public ProjectionComparer(Func<TSource, TKey> keySelector, IComparer<TKey> comparer)
+        {
+            this.keySelector = keySelector;
+            this.comparer = comparer;
+        }
+
+        public int Compare([AllowNull] TSource x, [AllowNull] TSource y)
+        {
+            return comparer.Compare(keySelector(x), keySelector(y));
+        }
+    }
+}
