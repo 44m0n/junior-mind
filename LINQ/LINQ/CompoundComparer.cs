35961c3435cce744e4a9204f241fe52c308ac1b5
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-25 15:36:37 +0200

    Create CompoundComparer.cs

---
+++ LINQ/LINQ/CompoundComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace LINQ
+{
+    class CompoundComparer<TSource> : IComparer<TSource>
+    {
+        private readonly IComparer<TSource> firstComparer;
+        private readonly IComparer<TSource> secondComparer;
+
+        public CompoundComparer(IComparer<TSource> firstComparer, IComparer<TSource> secondComparer)
+        {
+            this.firstComparer = firstComparer;
+            this.secondComparer = secondComparer;
+        }
+
+        public int Compare([AllowNull] TSource x, [AllowNull] TSource y)
+        {
+            return firstComparer.Compare(x, y) != 0 ? firstComparer.Compare(x, y) : secondComparer.Compare(x, y);
+        }
+    }
+}
