d643ad83c3a1967157bbbd8835f3770d8f2420d4
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-26 09:41:38 +0200

    Create Stock.cs

---
+++ LINQ/LINQ/Stock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    public class Stock : IEnumerable<Product>
+    {
+        private readonly IEnumerable<Product> products;
+
+        public Stock(Product[] products)
+        {
+            this.products = products;
+        }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
