5d5c9b770cbc5c133d839609dc4f8be6c7123105
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-26 09:34:26 +0200

    Create Product.cs

---
+++ LINQ/LINQ/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    public class Product
+    {
+        public Product (string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        public string Name { get; protected set; }
+
+        public int Quantity { get; protected set; }
+
+        public void Add(int no)
+        {
+            Quantity += no;
+        }
+
+        public void Substract(int no)
+        {
+            Quantity -= no;
+        }
+    }
+}
