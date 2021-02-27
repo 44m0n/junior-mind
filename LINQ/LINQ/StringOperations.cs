d4723234806c48e39ea7096bfa4ded42e4beba60
Aamon <33285359+44m0n@users.noreply.github.com>
2021-02-27 16:05:58 +0200

    Created StringFunctions class

---
+++ LINQ/LINQ/StringOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    public class StringOperations
+    {
+        private readonly string text;
+
+        public StringOperations(string text)
+        {
+            this.text = text;
+        }
+
+
+    }
+}
