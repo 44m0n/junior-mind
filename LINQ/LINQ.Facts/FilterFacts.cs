4f9c6306abe4dabede1f72ccbf47fe9d29d1a4d6
Aamon <33285359+44m0n@users.noreply.github.com>
2021-03-03 22:36:12 +0200

    Created OneOrMoreFeatures()

---
+++ LINQ/LINQ.Facts/FilterFacts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LINQ.Facts
+{
+    public class FilterFacts
+    {
+        [Fact]
+        public void OneOrMoreTest()
+        {
+            var feature = new Feature[] { new Feature(), new Feature(), new Feature() };
+            feature[0].Id = 1;
+            feature[1].Id = 3;
+            var feature2 = new Feature[] { new Feature() };
+            feature2[0].Id = 4;
+
+            var pr1 = new ProductWithFeatures();
+            pr1.Name = "produs1";
+            pr1.Features = feature;
+
+            var pr2 = new ProductWithFeatures();
+            pr2.Name = "produs2";
+            pr2.Features = feature2;
+
+            var products = new[] { pr1, pr2 };
+
+            Assert.Equal(new[] { pr2 }, Filter.OneOrMoreFeatures(products, feature2));
+        }
+    }
+}
---
+++ LINQ/LINQ/Filter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ
+{
+    public static class Filter
+    {
+        public static IEnumerable<ProductWithFeatures> OneOrMoreFeatures(ProductWithFeatures[] products, Feature[] features)
+        {
+            return from pr in products
+                         where features.Select(f => f.Id).Intersect(pr.Features.Select(pf => pf.Id)).Any()
+                         select pr;
+        }
+    }
+}
