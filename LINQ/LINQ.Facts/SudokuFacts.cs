e4c4fed2c7a4e36361b6e76204f8e681b0af504f
Aamon <33285359+44m0n@users.noreply.github.com>
2021-03-04 03:23:47 +0200

    Created Sudoku class

---
+++ LINQ/LINQ.Facts/SudokuFacts.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LINQ.Facts
+{
+    public class SudokuFacts
+    {
+        [Fact]
+        public void SudokuTest()
+        {
+            int[,] sudoku =
+            {
+                { 2,4,4,3,3,5,9,6,7 },
+                { 6,2,8,3,2,6,1,7,6 },
+                { 7,6,7,7,3,5,5,6,8 },
+                { 1,7,5,3,7,5,7,7,3 },
+                { 3,3,1,8,1,1,5,8,4 },
+                { 7,5,2,7,2,4,2,3,7 },
+                { 5,9,1,2,4,9,7,9,4 },
+                { 8,6,3,1,4,8,3,6,8 },
+                { 9,4,4,8,1,4,5,5,7 }
+            };
+
+            var obj = new Sudoku(sudoku);
+            Assert.False(obj.Validate());
+
+            sudoku = new int[,]
+            {
+                { 4, 8, 9, 2, 5, 7, 6, 3, 1, },
+                { 7, 1, 3, 4, 9, 6, 8, 5, 2, },
+                { 6, 2, 5, 3, 8, 1, 4, 9, 7, },
+                { 2, 6, 1, 9, 3, 8, 5, 7, 4, },
+                { 5, 3, 7, 1, 2, 4, 9, 8, 6, },
+                { 8, 9, 4, 7, 6, 5, 1, 2, 3, },
+                { 9, 4, 6, 8, 7, 3, 2, 1, 5, },
+                { 1, 7, 2, 5, 4, 9, 3, 6, 8, },
+                { 3, 5, 8, 6, 1, 2, 7, 4, 9, }
+            };
+
+            obj = new Sudoku(sudoku);
+            Assert.True(obj.Validate());
+        }
+    }
+}
---
+++ LINQ/LINQ/Sudoku.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ
+{
+    public class Sudoku
+    {
+        private const int Nine = 9;
+        private const int Three = 3;
+        private const int NoOfElements = 81;
+        private readonly int[,] sudoku;
+
+        public Sudoku(int[,] sudoku)
+        {
+            this.sudoku = sudoku;
+        }
+
+        public bool Validate()
+        {
+            CheckParameterIsNull(this.sudoku, nameof(sudoku));
+            if (sudoku.Length != NoOfElements)
+            {
+                throw new ArgumentException("The matrix should contains 81 elements");
+            }
+
+            return GetRows(sudoku).All(e => CheckSequence(e)) && GetColumns(sudoku).All(e => CheckSequence(e))
+                && GetSquares(sudoku).All(e => CheckSequence(e));
+        }
+
+        private IEnumerable<IEnumerable<int>> GetRows(int[,] source)
+        {
+            return Enumerable.Range(0, Nine).Select(i =>
+                        Enumerable.Range(0, Nine).Select(j => source[i, j]));
+        }
+
+        private IEnumerable<IEnumerable<int>> GetColumns(int[,] source)
+        {
+            return Enumerable.Range(0, Nine).Select(i =>
+                        Enumerable.Range(0, Nine).Select(j => source[j, i]));
+        }
+
+        private IEnumerable<IEnumerable<int>> GetSquares(int[,] source)
+        {
+            return Enumerable.Range(0, Nine).Select(i =>
+                        Enumerable.Range(0, Nine).Select(j =>
+                                source[i / Three * Three + j / Three, i % Three * Three + j % Three]));
+        }
+
+        private bool CheckSequence(IEnumerable<int> seq)
+        {
+            return !seq.GroupBy(el => el).Where(e => e.Count() > 1).Select(e => e).Any();
+        }
+
+        private void CheckParameterIsNull<T>(T param, string name)
+        {
+            if (param != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(paramName: name);
+        }
+    }
+}
