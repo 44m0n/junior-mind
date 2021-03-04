af566f977112c0c0f7000bfa8fb6232fb4182deb
Aamon <33285359+44m0n@users.noreply.github.com>
2021-03-04 13:18:26 +0200

    Create PolishPostfix.cs

---
+++ LINQ/LINQ/PolishPostfix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ
+{
+    public class PolishPostfix
+    {
+        private readonly string ec;
+
+        public PolishPostfix(string input)
+        {
+            CheckParameterIsNull(input, nameof(input));
+            ec = input;
+        }
+
+        private void CheckParameterIsNull<T>(T param, string name)
+        {
+            if (param != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(paramName: name);
+        }
+    }
+}
