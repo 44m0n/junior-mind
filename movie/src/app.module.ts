22cf7eaa59e077d97a03dd38d4600f4a7af0a022
Aamon <33285359+44m0n@users.noreply.github.com>
2021-03-12 11:19:26 +0200

    Updated app modules

--- movie/src/app.module.ts
+++ movie/src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloworldModule } from './helloworld/helloworld.module';
 import { MovieController } from './movie/movie.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 
 @Module({
-  imports: [HelloworldModule],
-  controllers: [AppController, MovieController],
-  providers: [AppService]
+    imports: [HelloworldModule, TypeOrmModule.forRoot()],
+    controllers: [AppController, MovieController],
+    providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private connection: Connection) { }
+}
