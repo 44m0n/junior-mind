01ec6fa0b6222575d1baa6f3ef56a5ca3d359bee
Aamon <33285359+44m0n@users.noreply.github.com>
2021-03-12 11:19:52 +0200

    Create the movie entity

---
+++ movie/src/movie/movie.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class MovieEntity {
+    @PrimaryGeneratedColumn()
+    Id: number;
+
+    @Column()
+    Title: string;
+
+    @Column()
+    ReleaseDate: Date;
+
+    @Column()
+    Genre: string;
+
+
+    @Column()
+    Price: number;
+}
