67418e4ee9abe2423877911688d49670773f1097
Aamon <33285359+44m0n@users.noreply.github.com>
2021-03-12 15:12:00 +0200

    Update

--- movie/src/app.module.ts
+++ movie/src/app.module.ts
@@ -5,10 +5,22 @@ import { HelloworldModule } from './helloworld/helloworld.module';
 import { MovieController } from './movie/movie.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import { MovieEntity } from './movie/movie.entity';
 
 
 @Module({
-    imports: [HelloworldModule, TypeOrmModule.forRoot()],
+    imports: [HelloworldModule, TypeOrmModule.forRoot(
+        {
+            type: "mysql",
+            host: "localhost",
+            port: 3306,
+            username: "root",
+            password: "toor",
+            database: "Mysql",
+            entities: [MovieEntity],
+            synchronize: true
+        }
+            )],
     controllers: [AppController, MovieController],
     providers: [AppService]
 })
