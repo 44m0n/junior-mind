fa7cd01f5a884fa24728b8d21d5ba03312276da5
Aamon <33285359+44m0n@users.noreply.github.com>
2021-03-15 15:56:57 +0200

    Update main.ts
    Registered the helpers

--- movie/src/main.ts
+++ movie/src/main.ts
@@ -2,15 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(
         AppModule,
     );
 
-    app.useStaticAssets(join(__dirname, '..', 'public'));
+    hbs.registerHelper("release", function (fullDate) {
+        var humanDate = new Date(fullDate).toDateString();
+        return humanDate;
+    })
+
+    app.useStaticAssets(join(__dirname, '..', 'src/public'));
     app.setBaseViewsDir(join(__dirname, '..', 'src/views'));
+   
     app.setViewEngine('hbs');
     await app.listen(3000);
+
 }
 bootstrap();
